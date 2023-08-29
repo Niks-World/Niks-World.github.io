2e77e1ac84d2b1d502ee844c1645ecf34682d810
Niks-World <nikkalyan52@gmail.com>
2023-08-29 05:43:30 +0530

    resume

--- js/index.js
+++ js/index.js
@@ -2,12 +2,12 @@ var resumeLink = document.querySelector("#resume-button-1");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/16xD4c0-ea6eBSpyIkeFk_MwQ44pjb9rq/view?usp=sharing",
+    "https://drive.google.com/file/d/1yHXIX5v7OTM4GDi4pu0Tr-MMG3LiTM7G/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/u/0/uc?id=16xD4c0-ea6eBSpyIkeFk_MwQ44pjb9rq&export=download";
+    "https://drive.google.com/uc?export=download&id=1yHXIX5v7OTM4GDi4pu0Tr-MMG3LiTM7G";
   link.download = "Hasbe-Alam-Resume";
   link.click();
 });
@@ -16,12 +16,12 @@ var resumeLink = document.querySelector("#resume-button-2");
 resumeLink.addEventListener("click", function (event) {
   event.preventDefault();
   window.open(
-    "https://drive.google.com/file/d/16xD4c0-ea6eBSpyIkeFk_MwQ44pjb9rq/view?usp=sharing",
+    "https://drive.google.com/file/d/1yHXIX5v7OTM4GDi4pu0Tr-MMG3LiTM7G/view?usp=sharing",
     "_blank"
   );
   let link = document.createElement("a");
   link.href =
-    "https://drive.google.com/u/0/uc?id=16xD4c0-ea6eBSpyIkeFk_MwQ44pjb9rq&export=download";
+    "https://drive.google.com/uc?export=download&id=1yHXIX5v7OTM4GDi4pu0Tr-MMG3LiTM7G";
   link.download = "Hasbe-Alam-Resume";
   link.click();
 });
